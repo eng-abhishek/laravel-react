a0c592fc9124929687476500270bc3ac8b53e3ee
Abhishek Soni <soni.abhishek@velocis.in>
2025-12-08 20:23:25 +0530

    set-up-react-laravel-project

---
+++ Backend/routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+
+Route::get('/message', function () {
+    return response()->json([
+        'message' => 'Hello from Laravel API!'
+    ]);
+});
